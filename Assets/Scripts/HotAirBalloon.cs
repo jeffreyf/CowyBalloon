5c225a68a3fa0bd159131b3e9a72337d7a4aa0d5
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2019-01-25 19:10:58 -0400

    Prototyping some balloon movement.

---
+++ Assets/Scripts/HotAirBalloon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotAirBalloon : MonoBehaviour
+{
+    public Vector3 movementSpeeds = Vector3.one;
+    protected Vector3 input;
+
+    protected Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = Input.GetAxisRaw("Horizontal") * Vector3.right + Input.GetAxisRaw("Vertical") * Vector3.forward;
+
+        if (Input.GetButton("Jump"))
+        {
+            input.y = 1.0f;
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 force = new Vector3(input.x * movementSpeeds.x, input.y * movementSpeeds.y, input.z * movementSpeeds.z) * Time.deltaTime;
+        rb.AddForce(force);
+    }
+}
