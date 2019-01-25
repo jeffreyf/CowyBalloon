ec2b2204db663aeebd500b692efb9b27d511db04
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2019-01-25 19:54:09 -0400

    Start of the collectible class. Also played around with phsyics a bit.

---
+++ Assets/Scripts/BalloonCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonCamera : MonoBehaviour
+{
+    // May take peak at the target's rb to get a sense of how far ahead the camera should move
+    public HotAirBalloon target;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Triggered by: " + other.name);
+
+        HotAirBalloon hotAirBalloon = other.GetComponentInParent<HotAirBalloon>();
+
+        if(hotAirBalloon)
+        {
+            Destroy(hotAirBalloon.gameObject);
+        }
+    }
+}
