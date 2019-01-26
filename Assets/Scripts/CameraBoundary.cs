9da111209dd3f41d30341341e66587450f41db16
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2019-01-26 10:36:28 -0400

    Baby bottle model and camera boundaries.

---
+++ Assets/Scripts/CameraBoundary.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBoundary : MonoBehaviour
+{
+    public Vector3 boundary = Vector3.left;
+
+    public bool onlyOnStart = false; // If there's no camera movement, set this to true.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetPosition();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!onlyOnStart)
+        {
+            SetPosition();
+        }
+    }
+
+    private void SetPosition()
+    {
+        Vector3 worldPosition = Camera.main.ViewportToWorldPoint(boundary);
+        worldPosition.z = transform.position.z;
+        transform.position = worldPosition;
+    }
+}
