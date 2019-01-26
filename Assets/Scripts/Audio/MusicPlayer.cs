c5023130c010b87a34b8dfc0e2da692286e6057a
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2019-01-26 14:03:59 -0400

    Adding music into the level. MusicPlayer.cs isn't functional yet.

---
+++ Assets/Scripts/Audio/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public List<AudioSource> audios;
+
+
+    public void SetVolumes(List<float> volumes)
+    {
+        for(int i = 0; i < volumes.Count && i < audios.Count; i++)
+        {
+            audios[i].volume = volumes[i];
+        }
+    }
+
+}
