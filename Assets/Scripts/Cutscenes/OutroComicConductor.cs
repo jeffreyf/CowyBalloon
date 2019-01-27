912c38a3e34817cc12e10914de5519f6c623e14c
tehdi <tehdi@users.noreply.github.com>
2019-01-27 13:54:55 -0400

    add outro comic

---
+++ Assets/Scripts/Cutscenes/OutroComicConductor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OutroComicConductor : MonoBehaviour
+{
+    public List<Sprite> ComicImages;
+    public Image ComicImage;
+
+    public string nextScene = "Gallery";
+
+    void Start()
+    {
+        StartCoroutine(PlayComic());
+    }
+
+    private IEnumerator PlayComic()
+    {
+        for (int i = 0; i < ComicImages.Count; i++)
+		{
+            ComicImage.sprite = ComicImages[i];
+            yield return new WaitForSeconds(2.0f);
+		}
+        
+        SceneManager.LoadScene(nextScene);
+    }
+}
