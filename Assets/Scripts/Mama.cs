363be2b3f52c60319a600323a75ee9d8cd64a915
tehdi <tehdi@users.noreply.github.com>
2019-01-27 08:32:47 -0400

    made Mama Cow moo when you land on her

---
+++ Assets/Scripts/Mama.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mama : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Balloon has hit Mama!");
+
+            PlayRandomlyChosenAudioClip audioPlayer = GetComponent<PlayRandomlyChosenAudioClip>();
+            if (audioPlayer)
+            {
+                audioPlayer.PlayAudio();
+            }
+            else
+            {
+                AudioSource audioSource = GetComponent<AudioSource>();
+                if (audioSource)
+                {
+                    audioSource.Play();
+                }
+            }
+        }
+    }
+}
