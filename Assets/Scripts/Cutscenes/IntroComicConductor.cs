9766c2ebf2172df8bc2563302ec8eec79b4548bc
tehdi <tehdi@users.noreply.github.com>
2019-01-27 12:58:33 -0400

    add intro scene with adorable comic

---
+++ Assets/Scripts/Cutscenes/IntroComicConductor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroComicConductor : MonoBehaviour
+{
+    public List<Sprite> ComicImages;
+    public Image ComicImage;
+    public Button SkipButton;
+
+    private string nextScene = "CowyBalloon";
+
+    void Start()
+    {
+        StartCoroutine(PlayComic());
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+
+    private IEnumerator PlayComic()
+    {
+        for (int i = 0; i < ComicImages.Count; i++)
+		{
+            ComicImage.sprite = ComicImages[i];
+            yield return new WaitForSeconds(2.0f);
+		}
+        
+        SceneManager.LoadScene(nextScene);
+    }
+}
